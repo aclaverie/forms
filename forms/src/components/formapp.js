5644cb39971da15525293bed3f8d671da8a65d37
Anthony Claverie <44935840+aclaverie@users.noreply.github.com>
2023-05-31 00:30:08 -0400

    have to separate the form to fix state

--- forms/src/components/formapp.js
+++ forms/src/components/formapp.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useState } from 'react';
-import {TextField, Checkbox, RadioGroup, Radio } from '@mui/material';
-import FormLabel from '@mui/material/FormLabel';
+import {Input, Checkbox, RadioGroup, Radio } from '@mui/material';
+import TextareaAutosize from '@mui/base/TextareaAutosize';
 import FormControl from '@mui/material/FormControl';
 import FormGroup from '@mui/material/FormGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
@@ -13,77 +13,169 @@ function FormApp() {
         firstName: "",
         lastName: "",
         email: "",
-        bike: false,
-        car: false,
-        boat: false,
-        train: true,
+        
         framework: "react",
-    })
+        concerns: "",
+        feedback: "",
+    });
+    const [transport, setTransport] = useState([
+        'walk : false',
+        'bike : false',
+        'car: false',
+        'boat: false',
+        'train: false',
+    ]);
 
+    function handleCheck(event) {
+        setTransport(prevTransport => {
+            const index = prevTransport.indexOf(event.target.value);
+            console.log(index);
+            return [...prevTransport]
+        })
+    }
 
-  return (
-    <div className="app-box">
-        <div className="app-notification">
-            <h4>Notifications</h4>
+    function handledChange(event){
+        setFormData(prevFormData => {
+            // let man = [prevFormData];
+            // switch ([event.target.name]) {
+            //     case 'walk':
+            //         console.log(event.target.checked)
+            //         man.walk = event.target.checked;
+            //         break;
+            //     case 'bike':
+            //         man.bike = event.target.checked;
+            //         break; 
+            //     case 'car':
+            //         man.car = event.target.checked;
+            //         break; 
+            //     case 'boat':
+            //         man.boat = event.target.checked;
+            //         break; 
+            //     case 'train':
+            //         man.train = event.target.checked;
+            //         break;
+            // }
+            // prevFormData = man;
+            return{
+                ...prevFormData,
+                [event.target.name]: event.target.value
+            }
+        })
+    }
+    console.log(formData.walk);
+    // const transport = [];
+    // transport['walk'] = formData.walk;
+    // transport['bike'] = formData.bike;
+    // transport['car'] = formData.car;
+    // transport['boat'] = formData.boat;
+    // transport['train'] = formData.train;
+
+    // let tran = "";
+    // console.log(transport.walk)
+    // const mark = transport.forEach((value)=>{  
+    //     console.log("LOL"+value)                           
+        // switch (value) {
+        //     case 'true':
+        //         console.log(key);
+        //         tran = key;
+        //         break;                            
+        //     default:
+        //         tran = 'no transport';
+        //         break;
+        // }
+        // return tran;
+    // })
+    // console.log(tran);
+    const filledForm =
+            <h6>
+                <p>Name: {formData.firstName +" "+ formData.lastName}</p>
+                <p>Email: {formData.email}</p>
+                <p>Walk: {formData.walk} Bike: {formData.bike} Car: {formData.car} Boat: {formData.boat} Train: {formData.train}</p>
+                <p></p>{
+                  +" "+
+                     +" "+
+                   +" "+
+                   +" "+
+                   +" "+
+                   +" "+
+                   +" "+
+                formData.framework +" "+
+                formData.concerns  +" "+
+                formData.feedback
+                }
+            </h6>
+                
+    return (
+        <div className="app-box">
+            <div className="app-notification">
+                {filledForm}
+            </div>
+            <form>
+                <fieldset className='personal'>
+                    <legend>Personal:</legend>
+                    <Input onChange={handledChange} placeholder="First Name" variant="outlined" name="firstName" defaultValue={formData.firstName} />
+                    <Input onChange={handledChange} placeholder="Last Name" variant="outlined" name="lastName" defaultValue={formData.lastName} />
+                    <Input onChange={handledChange} placeholder="Email" variant="outlined" name="email" defaultValue={formData.email} />
+                    {/*Think about using a calender here*/}
+                </fieldset>
+                <fieldset className='transport'>
+                    <legend>Transport:</legend>
+                    <FormControl sx={{ m: 1 }}>
+                        <FormGroup row>
+                            <FormControlLabel
+                            control={<Checkbox onChange={handleCheck} name="walk" defaultValue={transport.walk}    />}
+                            label='Walk' />
+                            <FormControlLabel
+                            control={<Checkbox onChange={handleCheck} name="bike" defaultValue={transport.bike}    />}
+                            label='Bike' />
+                            <FormControlLabel
+                            control={<Checkbox onChange={handleCheck} name="car" defaultValue={transport.car}     />}
+                            label='Car' />
+                            <FormControlLabel
+                            control={<Checkbox onChange={handleCheck} name="boat" defaultValue={transport.boat}    />}
+                            label='Boat' />
+                            <FormControlLabel
+                            control={<Checkbox onChange={handleCheck} name="train" defaultValue={transport.train}   />}
+                            label='Train' />
+                        </FormGroup>
+                    </FormControl>
+                </fieldset>
+                <fieldset className='framework'>
+                    <legend>Framework:</legend>
+                    <FormControl sx={{ m: 1 }}>
+                        {/* <RadioGroup row name='framework' defaultValue={formData.framework} onChange={handledChange} >
+                            <FormControlLabel control={<Radio defaultValue='react'   />}  label='React JS'   />
+                            <FormControlLabel control={<Radio defaultValue='vue'     />}  label='Vue'        />
+                            <FormControlLabel control={<Radio defaultValue='angular' />}  label='Angular JS' />
+                        </RadioGroup> */}
+                    </FormControl>               
+                </fieldset>
+                <fieldset className='concerns'>
+                    <legend>Concerns:</legend>
+                    {/* <FormControl sx={{ m: 1 }}> */}
+                        <TextareaAutosize
+                        onChange={handledChange}
+                        placeholder="Share your concerns"
+                        minRows={"4"} cols="100" 
+                        defaultValue={formData.concerns} 
+                        name="concerns"  />                        
+                    {/* </FormControl>      */}
+                </fieldset>
+                <fieldset className='feedback'>
+                    <legend>Feedback:</legend>
+                    <FormControl sx={{ m: 1 }}>
+                        <TextareaAutosize
+                        onChange={handledChange}
+                        placeholder="Tell us what you think of the process"  
+                        minRows={"4"} cols="100" 
+                        defaultValue={formData.feedback}
+                        name="feedback"  />                        
+                    </FormControl>
+                </fieldset>
+            </form>
         </div>
-        <form>
-            <fieldset className='personal'>
-                <legend>Personal:</legend>
-                <TextField type="text"  label="First Name" variant="outlined" name="firstName" value={formData.firstName} /><br /><br />
-                <TextField type="text"  label="Last Name" variant="outlined" name="lastName" value={formData.lastName} /><br /><br />
-                <TextField type="email" label="Email" variant="outlined" name="email" value={formData.email} />
-                {/*Think about using a calender here*/}
-            </fieldset>
-            <fieldset className='transport'>
-                <legend>Transport:</legend>
-                <FormControl sx={{ m: 1 }}>
-                    <FormGroup row>
-                        <FormControlLabel 
-                            control={<Checkbox type="checkbox" name="bike" value={formData.bike} />}
-                            label='Bike' >
-                        </FormControlLabel>
-                        <FormControlLabel 
-                            control={<Checkbox type="checkbox" name="car" value={formData.car} />}
-                            label='Car' >
-                        </FormControlLabel>
-                        <FormControlLabel 
-                            control={<Checkbox type="checkbox" name="boat" value={formData.boat} />}
-                            label='Boat' >
-                        </FormControlLabel>
-                        <FormControlLabel 
-                            control={<Checkbox type="checkbox" name="train" value={formData.train} />}
-                            label='Train' >
-                        </FormControlLabel>                    
-                    </FormGroup>
-                </FormControl>
-            </fieldset>
-            <fieldset className='framework'>
-                <legend>Framework:</legend>
-                <FormControl sx={{ m: 1 }}>
-                    <RadioGroup row name="framework" defaultValue={formData.framework} onChange={''}>
-                        <FormControlLabel control={<Radio />} value="react" label='React JS' />
-                        <FormControlLabel control={<Radio />} value="vue" label='Vue' />
-                        <FormControlLabel control={<Radio />} value="angular" label='Angular JS' />
-                    </RadioGroup>
-                </FormControl>               
-            </fieldset>
-            <fieldset className='comments'>
-                <legend>Concerns:</legend>
-                <textarea id="w3review" name="w3review" rows="4" cols="100">
-                    At w3schools.com you will learn how to make a website. 
-                    They offer free tutorials in all web development technologies.
-                </textarea>
-            </fieldset>
-            <fieldset className='feedback'>
-                <legend>Feedback:</legend>
-                <textarea id="w3review" name="w3review" rows="4" cols="100">
-                    At w3schools.com you will learn how to make a website. 
-                    They offer free tutorials in all web development technologies.
-                </textarea>
-            </fieldset>
-        </form>
-    </div>
-  )
+    )
 }
+
 
 export default FormApp;
