1375bf6873c11b0483835117aca0d5d8d8396ab4
Anthony Claverie <44935840+aclaverie@users.noreply.github.com>
2023-06-06 17:02:23 -0400

    have the processing, yet check box not right

--- forms/src/components/formapp.js
+++ forms/src/components/formapp.js
@@ -10,99 +10,48 @@ import FormControlLabel from '@mui/material/FormControlLabel';
 
 function FormApp() {
     const [formData, setFormData] = useState({
-        firstName: "",
-        lastName: "",
-        email: "",
-        
-        framework: "react",
-        concerns: "",
-        feedback: "",
+        firstName   : "",
+        lastName    : "",
+        email       : "",
+        walk        : false,
+        bike        : false,
+        car         : false,
+        boat        : false,
+        train       : false,
+        framework   : "react",
+        concerns    : "",
+        feedback    : "",
     });
-    const [transport, setTransport] = useState([
-        'walk : false',
-        'bike : false',
-        'car: false',
-        'boat: false',
-        'train: false',
-    ]);
+    const [transport, setTransport] = useState([]);
 
-    function handleCheck(event) {
-        setTransport(prevTransport => {
-            const index = prevTransport.indexOf(event.target.value);
-            console.log(index);
-            return [...prevTransport]
-        })
+    function handleCheck(e) {
+        console.log([e.target.name]+": "+e.target.value)
+        // setTransport(prevTransport => {
+        //     const index = prevTransport.indexOf(event.target.value);
+        //     console.log(index);
+        //     return [...prevTransport]
+        // })
     }
 
-    function handledChange(event){
+    function handledChange(e){
+        console.log([e.target.name]+": "+e.target.value)
         setFormData(prevFormData => {
-            // let man = [prevFormData];
-            // switch ([event.target.name]) {
-            //     case 'walk':
-            //         console.log(event.target.checked)
-            //         man.walk = event.target.checked;
-            //         break;
-            //     case 'bike':
-            //         man.bike = event.target.checked;
-            //         break; 
-            //     case 'car':
-            //         man.car = event.target.checked;
-            //         break; 
-            //     case 'boat':
-            //         man.boat = event.target.checked;
-            //         break; 
-            //     case 'train':
-            //         man.train = event.target.checked;
-            //         break;
-            // }
-            // prevFormData = man;
             return{
                 ...prevFormData,
-                [event.target.name]: event.target.value
+                [e.target.name]: e.target.value
             }
         })
     }
-    console.log(formData.walk);
-    // const transport = [];
-    // transport['walk'] = formData.walk;
-    // transport['bike'] = formData.bike;
-    // transport['car'] = formData.car;
-    // transport['boat'] = formData.boat;
-    // transport['train'] = formData.train;
-
-    // let tran = "";
-    // console.log(transport.walk)
-    // const mark = transport.forEach((value)=>{  
-    //     console.log("LOL"+value)                           
-        // switch (value) {
-        //     case 'true':
-        //         console.log(key);
-        //         tran = key;
-        //         break;                            
-        //     default:
-        //         tran = 'no transport';
-        //         break;
-        // }
-        // return tran;
-    // })
-    // console.log(tran);
+    
+   
     const filledForm =
             <h6>
                 <p>Name: {formData.firstName +" "+ formData.lastName}</p>
                 <p>Email: {formData.email}</p>
                 <p>Walk: {formData.walk} Bike: {formData.bike} Car: {formData.car} Boat: {formData.boat} Train: {formData.train}</p>
-                <p></p>{
-                  +" "+
-                     +" "+
-                   +" "+
-                   +" "+
-                   +" "+
-                   +" "+
-                   +" "+
-                formData.framework +" "+
-                formData.concerns  +" "+
-                formData.feedback
-                }
+                <p>Framework: {formData.framework}</p>
+                <p>Concerns: {formData.concerns}</p>
+                <p>Feedback: {formData.feedback}</p>
             </h6>
                 
     return (
@@ -123,31 +72,46 @@ function FormApp() {
                     <FormControl sx={{ m: 1 }}>
                         <FormGroup row>
                             <FormControlLabel
-                            control={<Checkbox onChange={handleCheck} name="walk" defaultValue={transport.walk}    />}
-                            label='Walk' />
+                            control={<Checkbox />}
+                            onChange={handledChange}
+                            value={transport.walk}  
+                            label='Walk'
+                            name='walk' />
                             <FormControlLabel
-                            control={<Checkbox onChange={handleCheck} name="bike" defaultValue={transport.bike}    />}
-                            label='Bike' />
+                            control={<Checkbox />}
+                            onChange={handledChange}
+                            value={transport.bike}
+                            label='Bike'
+                            name="bike" />
                             <FormControlLabel
-                            control={<Checkbox onChange={handleCheck} name="car" defaultValue={transport.car}     />}
-                            label='Car' />
+                            control={<Checkbox />}
+                            value={transport.car}
+                            onChange={handledChange}
+                            label='Car' 
+                            name="car" />
                             <FormControlLabel
-                            control={<Checkbox onChange={handleCheck} name="boat" defaultValue={transport.boat}    />}
-                            label='Boat' />
+                            control={<Checkbox />}
+                            onChange={handledChange}  
+                            value={transport.boat}    
+                            label='Boat'
+                            name="boat" />
                             <FormControlLabel
-                            control={<Checkbox onChange={handleCheck} name="train" defaultValue={transport.train}   />}
-                            label='Train' />
+                            control={<Checkbox />}
+                            onChange={handledChange}  
+                            value={transport.train}   
+                            label='Train' 
+                            name="train" />
                         </FormGroup>
                     </FormControl>
                 </fieldset>
                 <fieldset className='framework'>
                     <legend>Framework:</legend>
                     <FormControl sx={{ m: 1 }}>
-                        {/* <RadioGroup row name='framework' defaultValue={formData.framework} onChange={handledChange} >
-                            <FormControlLabel control={<Radio defaultValue='react'   />}  label='React JS'   />
-                            <FormControlLabel control={<Radio defaultValue='vue'     />}  label='Vue'        />
-                            <FormControlLabel control={<Radio defaultValue='angular' />}  label='Angular JS' />
-                        </RadioGroup> */}
+                        <RadioGroup row name='framework' value={formData.framework} onChange={handledChange} >
+                            <FormControlLabel control={<Radio />} value='react'   label='React JS'   />
+                            <FormControlLabel control={<Radio />} value='vue'     label='Vue'        />
+                            <FormControlLabel control={<Radio />} value='angular' label='Angular JS' />
+                        </RadioGroup>
                     </FormControl>               
                 </fieldset>
                 <fieldset className='concerns'>
